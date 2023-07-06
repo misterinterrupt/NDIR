f6959a17b656192dd6febb96d28088df9133734e
m. interrupt <misterinterrupt@gmail.com>
2023-07-05 17:12:40 -0700

    enable using any TwoWire i2c bus

--- NDIR_I2C.h
+++ NDIR_I2C.h
@@ -21,32 +21,37 @@ Please keep the above information when you use this code in your project.
 
 #ifndef _NDIR_I2C_H_
 #define _NDIR_I2C_H_
+#include <Wire.h>
 
-class NDIR_I2C {
-    public:
-		NDIR_I2C(uint8_t i2c_addr);
+class NDIR_I2C
+{
+public:
+    NDIR_I2C(TwoWire *i2c_bus, uint8_t i2c_addr);
 
-        uint8_t  i2c_addr;
-        uint32_t ppm;
+    uint8_t i2c_addr;
+    uint32_t ppm;
 
-        uint8_t  begin();
-        uint8_t  measure();
-        uint8_t  reset();
-        void     calibrateZero();
-        void     enableAutoCalibration();
-        void     disableAutoCalibration();
+    uint8_t begin();
+    uint8_t measure();
+    uint8_t reset();
+    void calibrateZero();
+    void enableAutoCalibration();
+    void disableAutoCalibration();
 
-    private:
-	    static uint8_t cmd_measure[9];
-        static uint8_t cmd_calibrateZero[9];
-        static uint8_t cmd_enableAutoCalibration[9];
-        static uint8_t cmd_disableAutoCalibration[9];
+protected:
+    TwoWire *I2C_BUS;
 
-        uint8_t  send(uint8_t *pdata, uint8_t n);
-        uint8_t  receive(uint8_t *pbuf, uint8_t n);
-        uint8_t  read_register(uint8_t reg_addr, uint8_t *pval);
-        uint8_t  write_register(uint8_t reg_addr, uint8_t *pdata, uint8_t n);
-        uint8_t  write_register(uint8_t reg_addr, uint8_t val);
-        uint8_t  parse (uint8_t *pbuf);
+private:
+    static uint8_t cmd_measure[9];
+    static uint8_t cmd_calibrateZero[9];
+    static uint8_t cmd_enableAutoCalibration[9];
+    static uint8_t cmd_disableAutoCalibration[9];
+
+    uint8_t send(uint8_t *pdata, uint8_t n);
+    uint8_t receive(uint8_t *pbuf, uint8_t n);
+    uint8_t read_register(uint8_t reg_addr, uint8_t *pval);
+    uint8_t write_register(uint8_t reg_addr, uint8_t *pdata, uint8_t n);
+    uint8_t write_register(uint8_t reg_addr, uint8_t val);
+    uint8_t parse(uint8_t *pbuf);
 };
 #endif
